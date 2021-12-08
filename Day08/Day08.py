4d677f27a3b4e7798431805ca2351d0b6260cbe0
adoni5 <roryjmunro1@gmail.com>
2021-12-08 07:24:10 +0000

    Day 8 part 1

---
+++ Day08/Day08.py
@@ -0,0 +1,35 @@
+import re
+from collections import Counter
+
+test_input = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb |
+fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec |
+fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef |
+cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega |
+efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga |
+gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf |
+gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf |
+cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd |
+ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg |
+gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc |
+fgae cfgab fg bagce"""
+
+with open("input") as fh:
+    test_input = fh.read()
+
+pat = re.compile(r"(?:\|\n|\| )(\w+) (\w+) (\w+) (\w+)")
+
+c = Counter()
+for digit in pat.findall(test_input):
+    for digi in digit:
+        c[len(digi)] += 1
+
+print(sum((c[i] for i in {2, 4, 3, 7})))
