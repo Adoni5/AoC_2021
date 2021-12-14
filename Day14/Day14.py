a08012b0080d6bf0ef489013804e7fd01b1a7240
adoni5 <roryjmunro1@gmail.com>
2021-12-14 12:16:25 +0000

    ✨✨✨ Part 2 - fast counters for fast money 🎁🎁🎁

--- Day14/Day14.py
+++ Day14/Day14.py
@@ -1,6 +1,7 @@
 import re
 from collections import Counter
-from itertools import pairwise
+from itertools import pairwise, chain
+
 test_input = """NNCB
 
 CH -> B
@@ -22,13 +23,20 @@ CN -> C"""
 with open("input") as fh:
     test_input = fh.read().strip()
 input, templates = test_input.split("\n\n")
+final_letter = input[-1]
 pat = re.compile(r"(\w+) -> (\w)")
-templates = dict(pat.findall(templates))
-for _ in range(10):
-    answer = []
-    for l1, l2 in pairwise(input):
-        answer.append(f"{l1}{templates[f'{l1}{l2}']}")
-    input = "".join(answer)
-    input += l2
-c = Counter(input)
-print(max(c.values()) - min(c.values()))
+templates = {inp: outp for inp, outp in pat.findall(templates)}
+input = pairwise(input)
+count = Counter("".join(p) for p in input)
+for _ in range(40):
+    print(_ + 1)
+    count2 = Counter()
+    for t, v in count.items():
+        count2[f"{t[0]}{templates[t]}"] += v
+        count2[f"{templates[t]}{t[1]}"] += v
+    count = count2
+monos = {k for k in chain.from_iterable(count.keys())}
+mono_counts = Counter()
+for m in monos:
+    mono_counts[m] = sum(v for c, v in count.items() if m == c[1])
+print(max(mono_counts.values()) - min(mono_counts.values()))
